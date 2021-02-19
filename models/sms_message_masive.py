f1c4184efec3026fde815e98e1f80aac3a2b7459
Levis <levislibra@hotmail.com>
2021-02-19 16:35:01 -0300

    Update

--- models/sms_message_masive.py
+++ models/sms_message_masive.py
@@ -17,7 +17,7 @@ class FinancieraSmsMessageMasive(models.Model):
 	partner_ids = fields.Many2many('res.partner', 'financiera_partner_messagemasive_rel', 'partner_id', 'masivemessage_id', string='Destinatarios')
 	tipo = fields.Char('Tipo de mensaje')
 	body_count_available = fields.Integer('Caracteres restantes', compute='_compute_body_count_available')
-	body = fields.Text('Mensaje')
+	body = fields.Text('Mensaje', size=160)
 	is_html = fields.Boolean('Adjuntar html')
 	html = fields.Text('Html')
 	company_id = fields.Many2one('res.company', 'Empresa')
@@ -47,23 +47,43 @@ class FinancieraSmsMessageMasive(models.Model):
 		})
 		return rec
 
-	@api.onchange('body')
+	@api.onchange('body', 'is_html')
 	def _compute_body_count_available(self):
 		if self.body:
-			self.body_count_available = 160 - len(self.body)
+			if not self.is_html:
+				self.body_count_available = 160 - len(self.body)
+			else:
+				self.body_count_available = 137 - len(self.body)
 		else:
-			self.body_count_available = 160
+			if not self.is_html:
+				self.body_count_available = 160
+			else:
+				self.body_count_available = 137
+
+	@api.constrains('body', 'is_html')
+	def _check_body(self):
+		if self.body:
+			if self.body_count_available < 0:
+				raise UserError("Debe borrar al menos %s caracteres."%str(abs(self.body_count_available)))
+
+
+	@api.one
+	def partners_deseleccionar(self):
+		self.partner_ids = [(6, 0, [])]
 
 	@api.one
 	def partners_cuota_preventiva(self):
 		cr = self.env.cr
 		uid = self.env.uid
+		cuota_obj = self.pool.get('financiera.prestamo.cuota')
+		cuota_ids = cuota_obj.search(cr, uid, [
+			('company_id', '=', self.company_id.id),
+			('state_mora', '=', 'preventiva'),
+			('state', '=', 'activa')])
 		partner_obj = self.pool.get('res.partner')
 		partner_ids = partner_obj.search(cr, uid, [
 			('company_id', '=', self.company_id.id),
-			('cuota_ids.state_mora', '=', 'preventiva'),
-			('cuota_ids.state', '=', 'activa')])
-		print("partners: ", partner_ids)
+			('cuota_ids.id', 'in', cuota_ids)])
 		self.partner_ids = [(6, 0, partner_ids)]
 
 	@api.one
@@ -71,29 +91,35 @@ class FinancieraSmsMessageMasive(models.Model):
 		cr = self.env.cr
 		uid = self.env.uid
 		fecha_actual = datetime.now()
+		cuota_obj = self.pool.get('financiera.prestamo.cuota')
+		cuota_ids = cuota_obj.search(cr, uid, [
+			('company_id', '=', self.company_id.id),
+			('fecha_vencimiento', '<', fecha_actual),
+			('state', '=', 'activa')])
 		partner_obj = self.pool.get('res.partner')
 		partner_ids = partner_obj.search(cr, uid, [
 			('company_id', '=', self.company_id.id),
-			('cuota_ids.fecha_vencimiento', '<', fecha_actual),
-			('cuota_ids.state', '=', 'activa')])
-		print("partners: ", partner_ids)
+			('cuota_ids.id', 'in', cuota_ids)])
 		self.partner_ids = [(6, 0, partner_ids)]
 
 	@api.one
 	def send_messages(self):
-		config_id = self.company_id.sms_configuracion_id
-		for partner_id in self.partner_ids:
-			sms_message_values = {
-				'partner_id': partner_id.id,
-				'config_id': config_id.id,
-				'to': partner_id.mobile,
-				'tipo': self.tipo or 'Personalizado',
-				'company_id': self.company_id.id,
-			}
-			message_id = self.env['financiera.sms.message'].create(sms_message_values)
-			message_id.body = self.body
-			if self.is_html:
-				message_id.html = self.html
-			message_id.send()
-			self.message_ids = [message_id.id]
-		self.state = 'send'
+		if len(self.partner_ids) > 0:
+			config_id = self.company_id.sms_configuracion_id
+			for partner_id in self.partner_ids:
+				sms_message_values = {
+					'partner_id': partner_id.id,
+					'config_id': config_id.id,
+					'to': partner_id.mobile,
+					'tipo': self.tipo or 'Personalizado',
+					'company_id': self.company_id.id,
+				}
+				message_id = self.env['financiera.sms.message'].create(sms_message_values)
+				message_id.body = self.body
+				if self.is_html:
+					message_id.html = self.html
+				message_id.send()
+				self.message_ids = [message_id.id]
+			self.state = 'send'
+		else:
+			raise UserError("Debe agregar al menos un destinatario.")
